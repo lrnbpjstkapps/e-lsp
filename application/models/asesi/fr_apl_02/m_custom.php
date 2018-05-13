7f5a3f6219e798ffb1b9518cab88538dfcffffe0
karidnr <39235748+karidnr@users.noreply.github.com>
2018-05-13 18:12:52 +0700

    FR-APL-02

---
+++ application/models/asesi/fr_apl_02/m_custom.php
@@ -0,0 +1,85 @@
+<?php
+	class m_custom extends CI_Model{		
+		
+		function getDt_FN105($uuid)
+			{				
+				$this->db->select('UK.UUID_UK, UK.JUDUL_UK, UK.KODE_UK');
+				$this->db->from("SKEMA_UK SUK");
+				$this->db->join("SKEMA AS SKE", "SUK.UUID_SKEMA = SKE.UUID_SKEMA", "LEFT");
+				$this->db->join("UNIT_KOMPETENSI AS UK", "SUK.UUID_UK = UK.UUID_UK", "LEFT");
+				$this->db->where('SUK.UUID_SKEMA', $uuid);
+				$this->db->where('SUK.IS_ACTIVE', '1');
+				$this->db->order_by("UK.KODE_UK", "ASC");
+				return $this->db->get();
+			}
+			
+		function getDt_FN105_FN134($uuid)
+			{				
+				$this->db->select('UK.UUID_UK');
+				$this->db->from("APL01_UK AS 01UK");
+				$this->db->join("UNIT_KOMPETENSI AS UK", "01UK.UUID_UK = UK.UUID_UK", "LEFT");
+				$this->db->where('01UK.UUID_APL01', $uuid);
+				$this->db->where('01UK.IS_ACTIVE', '1');
+				return $this->db->get();
+			}
+			
+		function getDt_FN142_FN134($uuid)
+			{				
+				$this->db->select('AB.UUID_BUKTI');
+				$this->db->from("APL01_BUKTI AS AB");
+				$this->db->join("BUKTI AS BUK", "AB.UUID_BUKTI = BUK.UUID_BUKTI", "LEFT");
+				$this->db->where('AB.UUID_APL01', $uuid);
+				$this->db->where('AB.IS_ACTIVE', '1');
+				return $this->db->get();
+			}
+			
+		function getDt_FN134($uuid)
+			{				
+				$this->db->select('APL01.UUID_APL01, APL01.UUID_ADM,  APL01.NO_DOKUMEN, APL01.NAMA_LENGKAP, 
+					APL01.TEMPAT_LAHIR, APL01.TGL_LAHIR, APL01.JENIS_KELAMIN, APL01.KEBANGSAAN,
+					APL01.ALAMAT_RUMAH, APL01.KODE_POS_RUMAH, APL01.NO_TLP_RUMAH, APL01.NO_TLP_HP, 
+					APL01.NO_TLP_KANTOR, APL01.EMAIL, APL01.PENDIDIKAN_TERAKHIR, APL01.NAMA_PERUSAHAAN, 
+					APL01.JABATAN, APL01.ALAMAT_KANTOR, APL01.KODE_POS_PERUSAHAAN, APL01.EMAIL_KANTOR, 
+					APL01.FAX_KANTOR, APL01.TUJUAN_ASESMEN, APL01.TUJUAN_ASESMEN_LAINNYA_KETERANGAN, 
+					APL01.JENIS_SKEMA, APL01.UUID_SKEMA, SKE.NAMA_SKEMA, SKE.NOMOR_SKEMA, APL01.DTM_CRT');
+				$this->db->from("FR_APL_01 AS APL01");
+				$this->db->join("SKEMA AS SKE", "APL01.UUID_SKEMA = SKE.UUID_SKEMA", "LEFT");
+				$this->db->where('APL01.UUID_USER', $uuid);
+				$this->db->where('APL01.IS_ACTIVE', '1');
+				return $this->db->get();
+			}
+			
+		function getADt_FN134($uuid)
+			{				
+				$this->db->select('APL01.UUID_APL01, APL01.UUID_ADM, APL01.NAMA_LENGKAP, 
+					APL01.TEMPAT_LAHIR, APL01.TGL_LAHIR, APL01.JENIS_KELAMIN, APL01.KEBANGSAAN,
+					APL01.ALAMAT_RUMAH, APL01.KODE_POS_RUMAH, APL01.NO_TLP_RUMAH, APL01.NO_TLP_HP, 
+					APL01.NO_TLP_KANTOR, APL01.EMAIL, APL01.PENDIDIKAN_TERAKHIR, APL01.NAMA_PERUSAHAAN, 
+					APL01.JABATAN, APL01.ALAMAT_KANTOR, APL01.KODE_POS_PERUSAHAAN, APL01.EMAIL_KANTOR, 
+					APL01.FAX_KANTOR, APL01.TUJUAN_ASESMEN, APL01.TUJUAN_ASESMEN_LAINNYA_KETERANGAN, 
+					APL01.JENIS_SKEMA, APL01.UUID_SKEMA, SKE.NAMA_SKEMA, SKE.NOMOR_SKEMA');
+				$this->db->from("FR_APL_01 AS APL01");
+				$this->db->join("SKEMA AS SKE", "APL01.UUID_SKEMA = SKE.UUID_SKEMA", "LEFT");
+				$this->db->where('APL01.UUID_APL01', $uuid);
+				$this->db->where('APL01.IS_ACTIVE', '1');
+				return $this->db->get();
+			}
+			
+		function getADt_FN134_AllJoinedTable($uuidApl01, $uuidSkema)
+			{				
+				$this->db->select('APL01.UUID_APL01, SKE.NOMOR_SKEMA, SKE.NAMA_SKEMA, 
+					UK.UUID_UK, UK.KODE_UK, UK.JUDUL_UK, EK.NOMOR_EK, EK.NAMA_EK, KUK.UUID_KUK, KUK.NOMOR_KUK, KUK.PERNYATAAN, KUK.PERTANYAAN');
+				$this->db->from("FR_APL_01 AS APL01");
+				$this->db->join("APL01_UK AS 01UK", "APL01.UUID_APL01 = 01UK.UUID_APL01", "LEFT");
+				$this->db->join("UNIT_KOMPETENSI AS UK", "01UK.UUID_UK = UK.UUID_UK ", "LEFT");
+				$this->db->join("SKEMA_UK AS SKEUK", "01UK.UUID_UK = SKEUK.UUID_UK", "LEFT");
+				$this->db->join("SKEMA AS SKE", "SKEUK.UUID_SKEMA = SKE.UUID_SKEMA", "LEFT");
+				$this->db->join("ELEMEN_KOMPETENSI AS EK", "01UK.UUID_UK = EK.UUID_UK", "LEFT");
+				$this->db->join("KRITERIA_UNJUK_KERJA AS KUK", "EK.UUID_EK = KUK.UUID_EK", "LEFT");
+				$this->db->where('APL01.UUID_APL01', $uuidApl01);
+				$this->db->where('SKE.UUID_SKEMA', $uuidSkema);
+				$this->db->where('APL01.IS_ACTIVE', '1');
+				return $this->db->get();
+			}
+	}
+?>
---
+++ application/views/asesi/v_fr_apl_01_form.php
@@ -0,0 +1,295 @@
+<div class="box box-info">
+	<div class="box-header with-border">
+	  <?php if($saveMethod=="add"){ ?>
+		  <h3 class="box-title"> <?php echo $menu_title[106]; ?> </h3>
+	  <?php }else{ ?>
+		  <h3 class="box-title"> <?php echo $menu_title[107]; ?> </h3>
+	  <?php } ?>
+	</div>
+	
+	<div class="box-body pad table-responsive">
+		<form id="<?php echo $form_id[136]; ?>" class="form-horizontal">
+			<div class="modal-body form">                
+				<input type="hidden" name="<?php echo $form_name[134]; ?>" value="<?php echo $$form_name[134]; ?>" id="<?php echo $form_id[139]; ?>"/> 
+				<div class="form-body">
+				<!-- Bagian 1 : Rincian Data Asesi -->
+					<h6 class="page-header">  <?php echo $form_desc[100]; ?> </h6>
+					<p class="text-muted well well-sm no-shadow"> <?php echo $form_desc[101]; ?> </p>		
+					
+				<!-- a. Data Pribadi -->
+					<h5 style="margin-bottom: 10px;"> <b> <?php echo $form_desc[102]; ?> </b> </h5>
+					<!-- Nama lengkap -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[124]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[115]; ?>" value="<?php echo $$form_name[115]; ?>" id="<?php echo $form_id[140]; ?>" class="form-control" type="text">
+							<span class="help-block"></span>
+						</div>
+					</div>	
+					
+					<!-- Tempat Lahir -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[125]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[116]; ?>" value="<?php echo $$form_name[116]; ?>" id="<?php echo $form_id[141]; ?>" class="form-control" type="text">
+							<span class="help-block"></span>
+						</div>
+					</div>	
+
+					<!-- Tanggal Lahir -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[140]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[117]; ?>" value="<?php echo $$form_name[117]; ?>" id="<?php echo $form_id[142]; ?>" class="form-control" placeholder="yyyy-mm-dd" data-date-format='yyyy-mm-dd'>
+							<span class="help-block"></span>
+						</div>
+					</div>						
+
+					<!-- Jenis kelamin -->
+					<div class="form-group">
+					<label class="control-label col-md-2"> <?php echo $form_label[126]; ?> </label>
+						<div class="col-md-4">
+							<div class="radio">
+								<?php if($$form_name[118] == $form_label[144]){ ?>									
+									<label>									
+										<input type="radio" name="<?php echo $form_name[118]; ?>" id="<?php echo $form_id[143]; ?>" value="<?php echo $form_label[143]; ?>">
+										<?php echo $form_label[143]; ?>
+									</label>
+									&nbsp
+									<label>
+										<input type="radio" name="<?php echo $form_name[118]; ?>" id="<?php echo $form_id[163]; ?>" value="<?php echo $form_label[144]; ?>" checked>
+										<?php echo $form_label[144]; ?>
+									</label>
+								<?php }else{ ?> 
+									<label>									
+										<input type="radio" name="<?php echo $form_name[118]; ?>" id="<?php echo $form_id[143]; ?>" value="<?php echo $form_label[143]; ?>" checked>
+										<?php echo $form_label[143]; ?>
+									</label>
+									&nbsp
+									<label>
+										<input type="radio" name="<?php echo $form_name[118]; ?>" id="<?php echo $form_id[163]; ?>" value="<?php echo $form_label[144]; ?>">
+										<?php echo $form_label[144]; ?>
+									</label>
+								<?php } ?>							
+							</div>
+						</div>		  
+					</div>	
+
+					<!-- Kebangsaan -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[127]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[119]; ?>" value="<?php echo $$form_name[119]; ?>" id="<?php echo $form_id[144]; ?>" class="form-control" type="text">
+							<span class="help-block"></span>
+						</div>
+					</div>	
+
+					<!-- Alamat Rumah -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[128]; ?> </label>
+						<div class="col-md-4">
+							<textarea name="<?php echo $form_name[120]; ?>" id="<?php echo $form_id[145]; ?>" class="form-control" rows="3" style="resize:none"><?php echo $$form_name[120]; ?></textarea>
+							<span class="help-block"></span>
+						</div>
+					</div>						
+					
+					<!-- Kode pos Rumah-->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[129]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[121]; ?>" value="<?php echo $$form_name[121]; ?>" id="<?php echo $form_id[146]; ?>" class="form-control" type="number">
+							<span class="help-block"></span>
+						</div>
+					</div>	
+					
+					<!-- No. Telepon Rumah -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[130]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[122]; ?>" value="<?php echo $$form_name[122]; ?>" id="<?php echo $form_id[147]; ?>" class="form-control" type="number">
+							<span class="help-block"></span>
+						</div>
+					</div>	
+					
+					<!-- HP -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[132]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[123]; ?>" value="<?php echo $$form_name[123]; ?>" id="<?php echo $form_id[148]; ?>" class="form-control" type="number">
+							<span class="help-block"></span>
+						</div>
+					</div>	
+					
+					<!-- E-mail Pribadi-->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[133]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[124]; ?>" value="<?php echo $$form_name[124]; ?>" id="<?php echo $form_id[149]; ?>" class="form-control" type="email">
+							<span class="help-block"></span>
+						</div>
+					</div>
+					
+					<!-- Pendidikan Terakhir -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[134]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[125]; ?>" value="<?php echo $$form_name[125]; ?>" id="<?php echo $form_id[150]; ?>" class="form-control" type="text">
+							<span class="help-block"></span>
+						</div>
+					</div>
+					
+				<!-- b. Data Pekerjaan Sekarang -->
+					</br><h5 style="margin-bottom: 10px;"> <b> <?php echo $form_desc[103]; ?> </b> </h5>
+					<!-- Nama Lembaga/Perusahaan -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[135]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[126]; ?>" value="<?php echo $$form_name[126]; ?>" id="<?php echo $form_id[151]; ?>" class="form-control" type="text">
+							<span class="help-block"></span>
+						</div>
+					</div>	
+
+					<!-- Jabatan -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[136]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[127]; ?>" value="<?php echo $$form_name[127]; ?>" id="<?php echo $form_id[152]; ?>" class="form-control" type="text">
+							<span class="help-block"></span>
+						</div>
+					</div>						
+					
+					<!-- Alamat kantor -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[137]; ?> </label>
+						<div class="col-md-4">
+							<textarea name="<?php echo $form_name[128]; ?>" id="<?php echo $form_id[153]; ?>" class="form-control" rows="3" style="resize:none"><?php echo $$form_name[128]; ?></textarea>
+							<span class="help-block"></span>
+						</div>
+					</div>	
+					
+					<!-- Kode Pos Kantor -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[142]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[129]; ?>" value="<?php echo $$form_name[129]; ?>" id="<?php echo $form_id[154]; ?>" class="form-control" type="number">
+							<span class="help-block"></span>
+						</div>
+					</div>
+					
+					<!-- No. Telepon Kantor -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[131]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[130]; ?>" value="<?php echo $$form_name[130]; ?>" id="<?php echo $form_id[155]; ?>" class="form-control" type="number">
+							<span class="help-block"></span>
+						</div>
+					</div>	
+					
+					<!-- Fax -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[138]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[131]; ?>" value="<?php echo $$form_name[131]; ?>" id="<?php echo $form_id[156]; ?>" class="form-control" type="number">
+							<span class="help-block"></span>
+						</div>
+					</div>
+					
+					<!-- E-mail Kantor -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[133]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[132]; ?>" value="<?php echo $$form_name[132]; ?>" id="<?php echo $form_id[157]; ?>" class="form-control" type="email">
+							<span class="help-block"></span>
+						</div>
+					</div>
+					
+				<!-- c. Data Permohonan Sertifikasi -->
+					</br><h5 style="margin-bottom: 10px;"> <b> <?php echo $form_desc[104]; ?> </b> </h5>
+					
+					<!-- Tujuan asesmen -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[139]; ?> </label>
+						<div class="col-md-7">
+							<input type="checkbox" name="<?php echo $form_name[133]; ?>" value="<?php echo $$form_name[133]; ?>" id="<?php echo $form_id[158]; ?>" value = "<?php echo $form_label[145]; ?>" disabled> <?php echo $form_label[145]; ?>
+							&nbsp <input type="checkbox" name="<?php echo $form_name[133]; ?>" value="<?php echo $$form_name[133]; ?>" id="<?php echo $form_id[159]; ?>" value = "<?php echo $form_label[146]; ?>" disabled> <?php echo $form_label[146]; ?>
+							&nbsp <input type="checkbox" name="<?php echo $form_name[133]; ?>" value="<?php echo $$form_name[133]; ?>" id="<?php echo $form_id[160]; ?>" value = "<?php echo $form_label[147]; ?>" disabled> <?php echo $form_label[147]; ?>
+							&nbsp <input type="checkbox" name="<?php echo $form_name[133]; ?>" value="<?php echo $$form_name[133]; ?>" id="<?php echo $form_id[161]; ?>" value = "<?php echo $form_label[148]; ?>" checked readonly> <?php echo $form_label[148]; ?>
+							&nbsp <input type="checkbox" name="<?php echo $form_name[133]; ?>" value="<?php echo $$form_name[133]; ?>" id="<?php echo $form_id[162]; ?>" value = "<?php echo $form_label[149]; ?>" disabled> <?php echo $form_label[149]; ?>
+							</br></br><input name="<?php echo $form_name[141]; ?>" value="<?php echo $$form_name[141]; ?>" id="<?php echo $form_id[164]; ?>" class="form-control" type="text" disabled>
+							<span class="help-block"></span>
+						</div>
+					</div>		
+					
+					<!-- Skema sertifikasi -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[141]; ?> </label>
+						<div class="col-md-4">
+							<select name="<?php echo $form_name[144]; ?>" value="<?php echo $$form_name[144]; ?>" id="<?php echo $form_id[165]; ?>" class="form-control" readonly>
+								<option value = "<?php echo $form_label[150]; ?>"> <?php echo $form_label[150]; ?> </option>
+								<option value = "<?php echo $form_label[151]; ?>" disabled readonly> <?php echo $form_label[151]; ?> </option>
+								<option value = "<?php echo $form_label[152]; ?>" disabled readonly> <?php echo $form_label[152]; ?> </option>
+							</select>
+						</div>
+					</div>	
+					
+				<!-- Bagian 2 : Daftar Unit Kompetensi -->
+					</br><h6 class="page-header">  <?php echo $form_desc[105]; ?> </h6>
+					<p class="text-muted well well-sm no-shadow"> <?php echo $form_desc[106]; ?> </p>
+					
+					<!-- Judul Skema sertifikasi -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[113]; ?> </label>
+						<div class="col-md-4">
+							<select name="<?php echo $form_name[100]; ?>" id="<?php echo $form_id[101]; ?>" onclick="setDt102()" class="form-control">
+								<?php foreach($listSkema->result() as $row){ ?>
+									<?php if($$form_name[102]==$row->UUID_SKEMA){?>
+										<option value = "<?php echo $row->UUID_SKEMA; ?>" selected><?php echo $row->NAMA_SKEMA; ?></option>
+									<?php }else{ ?>
+										<option value = "<?php echo $row->UUID_SKEMA; ?>"><?php echo $row->NAMA_SKEMA; ?></option>
+									<?php } ?>
+								<?php } ?>
+							</select>
+							<span class="help-block"></span>
+						</div>
+					</div>	
+
+					<!-- Nomor Skema sertifikasi -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[101]; ?> </label>
+						<div class="col-md-4">
+							<input name="<?php echo $form_name[101]; ?>" value="<?php echo $$form_name[101]; ?>" id="<?php echo $form_id[102]; ?>" class="form-control" type="text" readonly>
+							<span class="help-block"></span>
+						</div>
+					</div>
+					
+					<!-- Unit Kompetensi yang diujikan -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[115]; ?> </label>
+						<div class="col-md-6">
+							<select multiple="multiple" name="<?php echo $form_name[143]; ?>" value="<?php echo $$form_name[105]; ?>" id="<?php echo $form_id[115]; ?>"></select>
+						</div>
+					</div>	
+					
+				<!-- Bagian 3 : Bukti Kelengkapan Pemohon -->
+					</br><h6 class="page-header">  <?php echo $form_desc[107]; ?> </h6>
+					<p class="text-muted well well-sm no-shadow"> <?php echo $form_desc[108]; ?> 
+					</br></br> <?php echo $form_desc[109]; ?> </p>
+					
+					<!-- Bukti Kelengkapan Pemohon -->
+					<div class="form-group">
+						<label class="control-label col-md-2"> <?php echo $form_label[153]; ?> </label>
+						<div class="col-md-6">
+							<select multiple="multiple" name="<?php echo $form_name[139]; ?>" value="<?php echo $$form_name[136]; ?>" id="<?php echo $form_id[173]; ?>"></select>
+						</div>
+					</div>	
+					
+				</div>                
+			</div>
+		</form>	
+	</div>
+	
+	<div class="box-footer pad table-responsive">
+		<a onClick="pagingList()" class="btn bg-navy btn-default pull-right margin"> <?php echo $form_button[103]; ?> </a>
+		<a onClick="saveDt()" class="btn bg-navy btn-default pull-right margin"> <?php echo $form_button[101]; ?> </a>
+	</div>
+</div>
