35670ecb17a49703e85e1fa8bb585137ff3e153c
karidnr-office <karid.nurvenus@bpjsketenagakerjaan.go.id>
2018-07-21 21:58:25 +0700

    db & mak

--- application/views/asesi/v_fr_mak_02_form.php
+++ application/views/asesi/v_fr_mak_02_form.php
@@ -19,7 +19,7 @@
 						</tr>
 						<tr>
 							<td>Nomor</td>
-							<td><?= $$form_name[101]; ?></td>
+							<td>TBS</td>
 						</tr>
 						<tr>
 							<td>TUK</td>
@@ -30,13 +30,43 @@
 							<td colspan = "2">TBS</td>
 						</tr>
 						<tr>
+							<td>Nama Peserta</td>
+							<td colspan = "2">TBS</td>
+						</tr>
+						<tr>
 							<td>Tanggal</td>
 							<td colspan = "2">TBS</td>
 						</tr>
-					</table>						
+					</table>	
+
+					Asesor diminta untuk :
+					1. Mengkaji ulang dan menilai bukti kompetensi peserta yang dikumpulkan, apakah bukti kompetensi tersebut memenuhi aturan bukti Valid, Asli, Terkini dan memadai (VATM).
+					2. Membuat keputusan Asesmen atas penilaian bukti kompetensi Peserta, bila Peserta dinyatakan Kompeten tuliskan tanda v pada kolom (K), dan bila dinyatakan Belum Kompeten tuliskan tanda V pada kolom (BK) untuk setiap unit kompetensi sesuai dengan skema sertifikasi.
+					3. Memberikan umpan balik kepada Peserta mengenai pencapaian unjuk kerja dan Peserta juga diminta untuk memberikan umpan balik terhadap proses asesmen yang dilaksanakan (kuisioner).
+					4. Asesor dan Peserta bersama-sama menandatangani keputusan asesmen
+					
+					Pencapaian Kompetensi:
+					<table>
+						<tr>
+							<td rowspan = "2">No.</td>
+							<td rowspan = "2">Kode dan Judul Unit Kompetensi</td>
+							<td rowspan = "2">Bukti - bukti</td>
+							<td rowspan = "2">Jenis Bukti</td>
+							<td>Pencapaian</td>
+							<td>Keputusan</td>
+						</tr>
+						<tr>
+							<td>Ya</td>
+							<td>Tidak</td>
+							<td>K</td>
+							<td>BK</td>
+						</tr>
+					</table>
 				</div>   
 			</div>
 					
+				
+				
 			<div id="<?php echo $form_id[117]; ?>">
 				
 			</div>
